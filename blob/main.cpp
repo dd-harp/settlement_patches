3e9e26dd2859622acef1935689d3426774e083c2
Drew Dolgert <adolgert@uw.edu>
2019-08-04 14:25:51 -0700

    can run alpha complexes

--- blob/main.cpp
+++ blob/main.cpp
@@ -11,29 +11,32 @@
 #include <vector>
 #include "tiffio.h"
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef K::FT FT;
-typedef K::Point_2 Point;
-typedef K::Segment_2 Segment;
-typedef CGAL::Alpha_shape_vertex_base_2<K> Vb;
-typedef CGAL::Alpha_shape_face_base_2<K> Fb;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::FT FT;
+typedef Kernel::Point_2 Point;
+typedef Kernel::Segment_2 Segment;
+typedef CGAL::Alpha_shape_vertex_base_2<Kernel> Vb;
+typedef CGAL::Alpha_shape_face_base_2<Kernel> Fb;
 typedef CGAL::Triangulation_data_structure_2<Vb, Fb> Tds;
-typedef CGAL::Delaunay_triangulation_2<K, Tds> Triangulation_2;
+typedef CGAL::Delaunay_triangulation_2<Kernel, Tds> Triangulation_2;
 typedef CGAL::Alpha_shape_2<Triangulation_2> Alpha_shape_2;
 typedef Alpha_shape_2::Alpha_shape_edges_iterator Alpha_shape_edges_iterator;
 
 
 template<class OutputIterator>
-void alpha_edges(const Alpha_shape_2 &A, OutputIterator out) {
-    Alpha_shape_edges_iterator it = A.alpha_shape_edges_begin(),
-            end = A.alpha_shape_edges_end();
-    for (; it != end; ++it)
-        *out++ = A.segment(*it);
+void write_apha_edges(const Alpha_shape_2 &A, OutputIterator out) {
+    /*! Write each edge of an alpha shape into the output. */
+    auto it = A.alpha_shape_edges_begin();
+    auto end = A.alpha_shape_edges_end();
+    for (; it != end; ++it, ++out) {
+        *out = A.segment(*it);
+    }
 }
 
 
-template<class OutputIterator>
-bool tiff_input(OutputIterator out, const std::string& filename, double cutoff) {
+template<class PointOutputIterator>
+bool tiff_input(PointOutputIterator out, const std::string& filename, double cutoff) {
+    /*! Loop through a TIFF file, tile by tile, and insert values into output. */
     TIFF* tif = TIFFOpen(filename.c_str(), "r");
     if (nullptr == tif) {
         return false;
@@ -59,7 +62,7 @@ bool tiff_input(OutputIterator out, const std::string& filename, double cutoff) 
             for (uint32 j=0; j<tileLength; j++) {
                 for (uint32 i=0; i<tileWidth; i++) {
                     if (buffer[j*tileWidth + i] > cutoff) {
-                        out << Point(i + x, j+y);
+                        *out++ = Point(i + x, j+y);
                     }
                 }
             }
@@ -92,13 +95,15 @@ int main() {
     std::list<Point> points;
     if (!tiff_input(std::back_inserter(points), "hrsl_uga_pop.tif", 1.0))
         return -1;
-    Alpha_shape_2 A(points.begin(), points.end(),
-                    FT(10000),
-                    Alpha_shape_2::GENERAL);
-    std::vector<Segment> segments;
-    alpha_edges(A, std::back_inserter(segments));
+    Alpha_shape_2 alpha_shape(
+            points.begin(), points.end(),
+            FT(10000),
+            Alpha_shape_2::GENERAL
+    );
+    std::vector <Segment> segments;
+    write_apha_edges(alpha_shape, std::back_inserter(segments));
     std::cout << "Alpha Shape computed" << std::endl;
     std::cout << segments.size() << " alpha shape edges" << std::endl;
-    std::cout << "Optimal alpha: " << *A.find_optimal_alpha(1) << std::endl;
+    std::cout << "Optimal alpha: " << *alpha_shape.find_optimal_alpha(1) << std::endl;
     return 0;
 }

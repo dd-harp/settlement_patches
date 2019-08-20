1da647a105a4cc868e2c2841f9108096b455e0b7
Drew Dolgert <adolgert@uw.edu>
2019-08-19 22:22:25 -0700

    some points progress

--- blob/simple_patches.h
+++ blob/simple_patches.h
@@ -5,25 +5,29 @@
 #ifndef BLOB_SIMPLE_PATCHES_H
 #define BLOB_SIMPLE_PATCHES_H
 
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Alpha_shape_2.h>
-#include <CGAL/Alpha_shape_vertex_base_2.h>
-#include <CGAL/Alpha_shape_face_base_2.h>
-#include <CGAL/Delaunay_triangulation_2.h>
-#include <CGAL/Constrained_Delaunay_triangulation_2.h>
-#include <CGAL/Constrained_triangulation_plus_2.h>
-#include <CGAL/Polyline_simplification_2/simplify.h>
-#include <CGAL/Polyline_simplification_2/Squared_distance_cost.h>
-#include <CGAL/algorithm.h>
-#include <CGAL/assertions.h>
-#include <boost/pending/disjoint_sets.hpp>
-#include <boost/property_map/property_map.hpp>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <list>
-#include <vector>
 #include <map>
-#include <cmath>
+#include <set>
+#include <vector>
+
+#include <boost/pending/disjoint_sets.hpp>
+#include <boost/property_map/property_map.hpp>
+
+#include "CGAL/Exact_predicates_inexact_constructions_kernel.h"
+#include "CGAL/Alpha_shape_2.h"
+#include "CGAL/Alpha_shape_vertex_base_2.h"
+#include "CGAL/Alpha_shape_face_base_2.h"
+#include "CGAL/Delaunay_triangulation_2.h"
+#include "CGAL/Constrained_Delaunay_triangulation_2.h"
+#include "CGAL/Constrained_triangulation_plus_2.h"
+#include "CGAL/Polyline_simplification_2/simplify.h"
+#include "CGAL/Polyline_simplification_2/Squared_distance_cost.h"
+#include "CGAL/algorithm.h"
+#include "CGAL/assertions.h"
+
 #include "tiffio.h"
 #include "read_tiff.h"
 #include "pixel.h"
@@ -35,6 +39,7 @@ namespace spacepop {
     template<typename PIXELS>
     Polygon_2 pixels_to_polygon(PIXELS pixels, int scan_length, double alpha) {
         typedef PolygonKernel::Point_2 Point;
+        typedef PolygonKernel::Segment_2 Segment;
         // Set up the alpha shapes
         typedef CGAL::Alpha_shape_vertex_base_2<PolygonKernel> Vb;
         typedef CGAL::Alpha_shape_face_base_2<PolygonKernel> Fb;
@@ -49,6 +54,7 @@ namespace spacepop {
         for (auto &pixel: pixels) {
             auto [x, y] = PixelToPoint(pixel, scan_length);
             // It's a uint32 coming in. We need to use real coordinates near this line.
+            std::cout << "pixel (" << x << ", " << y << ")" << std::endl;
             pixel_bounds.emplace_back(Point(x, y));
             pixel_bounds.emplace_back(Point(x + 1, y));
             pixel_bounds.emplace_back(Point(x, y + 1));
@@ -60,16 +66,77 @@ namespace spacepop {
                 PolygonKernel::FT(alpha),
                 Alpha_shape_2::GENERAL
         );
-//        std::cout << "solid components " << complex.number_of_solid_components(alpha) << std::endl;
-//        std::cout << complex << std::endl;
-        Polygon_2 polygon;
-        // value of this iterator is Dt::vertex_handle;
-        auto vertex = complex.alpha_shape_vertices_begin();
-        auto vertex_end = complex.alpha_shape_vertices_end();
-        for (; vertex != vertex_end; ++vertex) {
-            ; // polygon.insert(polygon.vertices_end(), vertex->Point());
+        std::set<Point> points;
+        auto all_vertex = complex.alpha_shape_vertices_begin();
+        auto all_vertex_end = complex.alpha_shape_vertices_end();
+        for (; all_vertex != all_vertex_end; ++all_vertex) {
+            points.insert((*all_vertex)->point());
         }
-        return polygon;
+        std::cout << "found " << points.size() << " points" << std::endl;
+
+        std::multimap<Point,Point> point_to_segment;
+        auto all_edge = complex.alpha_shape_edges_begin();
+        auto all_edge_end = complex.alpha_shape_edges_end();
+        for (; all_edge != all_edge_end; ++all_edge) {
+            if (complex.classify(*all_edge) == Alpha_shape_2::REGULAR) {
+                auto segment = complex.segment(*all_edge);
+                point_to_segment.insert(std::make_pair(segment.source(), segment.target()));
+                point_to_segment.insert(std::make_pair(segment.target(), segment.source()));
+            }
+        }
+        std::cout << "found " << point_to_segment.size() << " segments" << std::endl;
+
+        std::vector<Polygon_2> polygon;
+        while (!points.empty()) {
+            Point seed_point;
+            bool finding_seed{true};
+            while (finding_seed && !points.empty()) {
+                auto seed_iter = points.begin();
+                seed_point = *seed_iter;
+                points.erase(seed_iter);
+                auto search_point = point_to_segment.equal_range(seed_point);
+                auto neighboring_points = std::distance(search_point.first, search_point.second);
+                if (neighboring_points == 2) {
+                    finding_seed = false;
+                } else {
+                    std::cout << "search point " << seed_point << " distance " << neighboring_points << std::endl;
+                }
+            }
+
+            bool looking_for_seed{true};
+            auto current_point = seed_point;
+            std::vector<Point> polypoints;
+            while (!points.empty() && looking_for_seed) {
+                polypoints.emplace_back(current_point);
+                auto search_segment = point_to_segment.equal_range(current_point);
+                auto search_point = search_segment.first;
+                auto next_point{current_point};
+                for (; search_point != search_segment.second; ++search_point) {
+                    auto found_point = search_point->second;
+                    if (polypoints.size() > 1) {
+                        if (polypoints[polypoints.size() - 2] != found_point) {
+                            next_point = found_point;
+                        }
+                    } else {
+                        next_point = found_point;
+                    }
+                }
+                if (next_point != current_point) {
+                    looking_for_seed = false;
+                    std::cout << "didn't find a next point " << current_point << std::endl;
+                } else if (current_point == seed_point) {
+                    std::cout << "found seed " << seed_point << std::endl;
+                    looking_for_seed = false;
+                }
+            }
+            polygon.emplace_back(Polygon_2{polypoints.begin(), polypoints.end()});
+        }
+        if (polygon.empty()) {
+            polygon.emplace_back(Polygon_2{});
+        } else if (polygon.size() > 1) {
+            std::cout << "Polygon has " << polygon.size() << " parts" << std::endl;
+        }
+        return polygon[0];
     }
 
     template<typename PIXELSETMAP>
--- blob/simple_patches_test.cpp
+++ blob/simple_patches_test.cpp
@@ -34,3 +34,19 @@ TEST(PixelsToPolygons, NeighboringUpDownPixels) {
     auto poly = pixels_to_polygon(points, 100, 4);
     ASSERT_EQ(poly.size(), 6);
 }
+
+
+TEST(PixelsToPolygons, HasAHole) {
+    vector<size_t> points;
+    int n{20};
+    for (int horizontal=0; horizontal < n; horizontal++) {
+        points.push_back(horizontal);
+        points.push_back((n - 1) * 100 + horizontal);
+    }
+
+    for (int vertical=1; vertical < n - 1; vertical++) {
+        points.push_back(vertical * 100);
+        points.push_back(vertical * 100 + n - 1);
+    }
+    auto poly = pixels_to_polygon(points, 100, 8);
+}
